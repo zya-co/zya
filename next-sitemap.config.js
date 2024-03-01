077561872bd112bba1bbe9aeceea178d37f12df3
saiminh <saiminh@gmail.com>
2024-03-01 12:30:25 +0100

    add sitemap gen

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://zya.co',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
