d2519da1702a033eb629020dde7094ceca790cd3
saiminh <saiminh@gmail.com>
2025-02-12 12:30:39 +0100

    bugfix

--- components/blocks/LatestBlogPosts.tsx
+++ components/blocks/LatestBlogPosts.tsx
@@ -102,7 +102,7 @@ export default function LatestBlogPosts({data, posts}) {
           {data.showMore && !isCollapsed && posts.map((post, i: number) => (
               i >= 3 &&
                 <div key={i} className={styles.post}>
-                  <Link href={`/blog/${post.filename} ?? '#'`}>
+                  <Link href={`/blog/${post.filename ?? '#'}`}>
                     <h3 className={styles.title}>{post.title ? post.title : 'No title'}</h3>
                   </Link>
                   <div className={styles.footer}>
