f7a1d2765d7ee4f20d4ff4ef41fda51ccecd58ea
saiminh <saiminh@gmail.com>
2025-02-21 10:20:16 +0100

    form success fail handling

--- components/blocks/ContactFormBlock.tsx
+++ components/blocks/ContactFormBlock.tsx
@@ -1,18 +1,57 @@
+'use client';
 import styles from './ContactFormBlock.module.css';
 import Button from '../Button';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 export function ContactFormBlock({data}) {
 
-  const handleFormSubmit = async (event) => {
+  const [isClient, setIsClient] = useState(false);
+
+  const [status, setStatus] = useState('');
+  const [error, setError] = useState('');
+
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) return null;
+
+  // const handleFormSubmit = async (event) => {
+  //   event.preventDefault();
+  //   const formData = new FormData(event.target) as any;
+  //   await fetch("/__forms.html", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  //     body: new URLSearchParams(formData).toString(),
+  //   });
+  //   // Success and error handling ...
+  // };
+
+  const handleFormSubmit = async (event: any) => {
     event.preventDefault();
-    const formData = new FormData(event.target) as any;
-    await fetch("/__forms.html", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
-    });
-    // Success and error handling ...
+    try {
+        setStatus('pending');
+        setError('');
+        const myForm = event.target as any;
+        const formData = new FormData(myForm) as any;
+        const urlSearchParams = new URLSearchParams(formData) as any ;
+        const res = await fetch('/__forms.html', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+          body: urlSearchParams.toString(),
+        });
+        if (res.status === 200) {
+          setStatus('ok');
+        } else {
+          setStatus('error');
+          setError(`${res.status} ${res.statusText}`);
+        }
+    } catch (e) {
+        setStatus('error');
+        setError(`${e}`);
+    }
   };
 
   return (
@@ -45,9 +84,56 @@ export function ContactFormBlock({data}) {
           />
           <label htmlFor="agree" className={styles.contactform__field__row__ppcheck}><input type="checkbox" name="agree" id="agree" required /> <span>Agree to our <Link scroll={false} href='/privacy-policy'>privacy policy</Link></span></label>
         </div>
+        {status === 'ok' && (
+              <div className={`${styles.alert} ${styles.alert_success}`}>
+                  <SuccessIcon />
+                  Submitted!
+              </div>
+          )}
+          {status === 'error' && (
+              <div className={`${styles.alert} ${styles.alert_error}`}>
+                  <ErrorIcon />
+                  {error}
+              </div>
+          )}
       </form>
     </div>
   )
+}
+
+function SuccessIcon() {
+  return (
+      <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="stroke-current shrink-0 h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+      >
+          <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+      </svg>
+  );
+}
+function ErrorIcon(success: any) {
+  return (
+      <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="stroke-current shrink-0 h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+      >
+          <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+      </svg>
+  );
 }
 
 export const contactFormBlockSchema = {
